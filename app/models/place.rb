f1b9eab3bd3a2ce3aafae3e02be211d57e5d79da
Ryan Johnson <rjohnson4444@live.com>
2015-05-18 09:53:08 -0600

    Generated places scaffold

---
+++ app/models/place.rb
@@ -0,0 +1,8 @@
+class Place < ActiveRecord::Base
+	validates_presence_of :name
+	validates_presence_of :phone
+	validates_presence_of :website
+	validates_presence_of :user_id
+	validates_presence_of :address
+	belongs_to :user
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :places
+
   devise_for :users, controllers: { registrations: "registrations"}
   
   # The priority is based upon order of creation: first created -> highest priority.
